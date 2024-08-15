b881e5313dc183b73b74d80c2764ba271b22f4b3
EmperorOnGit <emperoreditzyt@gmail.com>
2024-08-14 20:20:02 -0500

    Add files via upload

---
+++ passgen/passgen/skibitty toilet.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace PasswordGenerator
+{
+    partial class PasswordGeneratorForm
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.NumericUpDown PasswordLengthNumericUpDown;
+        private System.Windows.Forms.Button GeneratePasswordButton;
+        private System.Windows.Forms.TextBox PasswordTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox UppercaseCheckBox;
+        private System.Windows.Forms.CheckBox LowercaseCheckBox;
+        private System.Windows.Forms.CheckBox NumbersCheckBox;
+        private System.Windows.Forms.CheckBox SpecialCharactersCheckBox;
+        private System.Windows.Forms.Button CopyPasswordButton; 
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.PasswordLengthNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.GeneratePasswordButton = new System.Windows.Forms.Button();
+            this.PasswordTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.UppercaseCheckBox = new System.Windows.Forms.CheckBox();
+            this.LowercaseCheckBox = new System.Windows.Forms.CheckBox();
+            this.NumbersCheckBox = new System.Windows.Forms.CheckBox();
+            this.SpecialCharactersCheckBox = new System.Windows.Forms.CheckBox();
+            this.CopyPasswordButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.PasswordLengthNumericUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PasswordLengthNumericUpDown
+            // 
+            this.PasswordLengthNumericUpDown.Location = new System.Drawing.Point(12, 25);
+            this.PasswordLengthNumericUpDown.Minimum = new decimal(new int[] {
+            8,
+            0,
+            0,
+            0});
+            this.PasswordLengthNumericUpDown.Name = "PasswordLengthNumericUpDown";
+            this.PasswordLengthNumericUpDown.Size = new System.Drawing.Size(120, 20);
+            this.PasswordLengthNumericUpDown.TabIndex = 0;
+            this.PasswordLengthNumericUpDown.Value = new decimal(new int[] {
+            12,
+            0,
+            0,
+            0});
+            // 
+            // GeneratePasswordButton
+            // 
+            this.GeneratePasswordButton.Location = new System.Drawing.Point(138, 25);
+            this.GeneratePasswordButton.Name = "GeneratePasswordButton";
+            this.GeneratePasswordButton.Size = new System.Drawing.Size(120, 23);
+            this.GeneratePasswordButton.TabIndex = 1;
+            this.GeneratePasswordButton.Text = "Generate Password";
+            this.GeneratePasswordButton.UseVisualStyleBackColor = true;
+            this.GeneratePasswordButton.Click += new System.EventHandler(this.GeneratePasswordButton_Click);
+            // 
+            // PasswordTextBox
+            // 
+            this.PasswordTextBox.Location = new System.Drawing.Point(12, 154);
+            this.PasswordTextBox.Name = "PasswordTextBox";
+            this.PasswordTextBox.ReadOnly = true;
+            this.PasswordTextBox.Size = new System.Drawing.Size(246, 20);
+            this.PasswordTextBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Password Length:";
+            // 
+            // UppercaseCheckBox
+            // 
+            this.UppercaseCheckBox.AutoSize = true;
+            this.UppercaseCheckBox.Location = new System.Drawing.Point(12, 51);
+            this.UppercaseCheckBox.Name = "UppercaseCheckBox";
+            this.UppercaseCheckBox.Size = new System.Drawing.Size(78, 17);
+            this.UppercaseCheckBox.TabIndex = 4;
+            this.UppercaseCheckBox.Text = "Uppercase";
+            this.UppercaseCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // LowercaseCheckBox
+            // 
+            this.LowercaseCheckBox.AutoSize = true;
+            this.LowercaseCheckBox.Location = new System.Drawing.Point(12, 74);
+            this.LowercaseCheckBox.Name = "LowercaseCheckBox";
+            this.LowercaseCheckBox.Size = new System.Drawing.Size(78, 17);
+            this.LowercaseCheckBox.TabIndex = 5;
+            this.LowercaseCheckBox.Text = "Lowercase";
+            this.LowercaseCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // NumbersCheckBox
+            // 
+            this.NumbersCheckBox.AutoSize = true;
+            this.NumbersCheckBox.Location = new System.Drawing.Point(12, 97);
+            this.NumbersCheckBox.Name = "NumbersCheckBox";
+            this.NumbersCheckBox.Size = new System.Drawing.Size(68, 17);
+            this.NumbersCheckBox.TabIndex = 6;
+            this.NumbersCheckBox.Text = "Numbers";
+            this.NumbersCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // SpecialCharactersCheckBox
+            // 
+            this.SpecialCharactersCheckBox.AutoSize = true;
+            this.SpecialCharactersCheckBox.Location = new System.Drawing.Point(12, 120);
+            this.SpecialCharactersCheckBox.Name = "SpecialCharactersCheckBox";
+            this.SpecialCharactersCheckBox.Size = new System.Drawing.Size(115, 17);
+            this.SpecialCharactersCheckBox.TabIndex = 7;
+            this.SpecialCharactersCheckBox.Text = "Special Characters";
+            this.SpecialCharactersCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // CopyPasswordButton
+            // 
+            this.CopyPasswordButton.Location = new System.Drawing.Point(138, 54);
+            this.CopyPasswordButton.Name = "CopyPasswordButton";
+            this.CopyPasswordButton.Size = new System.Drawing.Size(120, 23);
+            this.CopyPasswordButton.TabIndex = 8;
+            this.CopyPasswordButton.Text = "Copy Password";
+            this.CopyPasswordButton.UseVisualStyleBackColor = true;
+            this.CopyPasswordButton.Click += new System.EventHandler(this.CopyPasswordButton_Click);
+            // 
+            // PasswordGeneratorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(314, 190);
+            this.Controls.Add(this.CopyPasswordButton);
+            this.Controls.Add(this.SpecialCharactersCheckBox);
+            this.Controls.Add(this.NumbersCheckBox);
+            this.Controls.Add(this.LowercaseCheckBox);
+            this.Controls.Add(this.UppercaseCheckBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PasswordTextBox);
+            this.Controls.Add(this.GeneratePasswordButton);
+            this.Controls.Add(this.PasswordLengthNumericUpDown);
+            this.Name = "PasswordGeneratorForm";
+            this.Text = "Passgen By Emperor";
+            ((System.ComponentModel.ISupportInitialize)(this.PasswordLengthNumericUpDown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+    }
+}
---
+++ passgen/passgen/skibitty toilet.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PasswordGenerator
+{
+    public partial class PasswordGeneratorForm : Form
+    {
+        private const string UppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        private const string LowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
+        private const string Numbers = "0123456789";
+        private const string SpecialCharacters = "!@#$%^&*()_+";
+
+        public PasswordGeneratorForm()
+        {
+            InitializeComponent();
+            ApplyDarkMode();
+        }
+
+        private void ApplyDarkMode()
+        {
+            this.BackColor = System.Drawing.Color.FromArgb(30, 30, 30);
+            this.ForeColor = System.Drawing.Color.White;
+
+            PasswordLengthNumericUpDown.BackColor = System.Drawing.Color.FromArgb(45, 45, 48);
+            PasswordLengthNumericUpDown.ForeColor = System.Drawing.Color.White;
+
+            GeneratePasswordButton.BackColor = System.Drawing.Color.FromArgb(50, 50, 50); // Dark gray
+            GeneratePasswordButton.ForeColor = System.Drawing.Color.White;
+
+            CopyPasswordButton.BackColor = System.Drawing.Color.FromArgb(50, 50, 50); // Dark gray
+            CopyPasswordButton.ForeColor = System.Drawing.Color.White;
+
+            PasswordTextBox.BackColor = System.Drawing.Color.FromArgb(45, 45, 48);
+            PasswordTextBox.ForeColor = System.Drawing.Color.White;
+
+            label1.ForeColor = System.Drawing.Color.White;
+        }
+
+        private void GeneratePasswordButton_Click(object sender, EventArgs e)
+        {
+            int passwordLength = (int)PasswordLengthNumericUpDown.Value;
+
+            if (passwordLength < 8)
+            {
+                MessageBox.Show("Please enter a valid password length of at least 8 characters.", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            bool includeUppercase = UppercaseCheckBox.Checked;
+            bool includeLowercase = LowercaseCheckBox.Checked;
+            bool includeNumbers = NumbersCheckBox.Checked;
+            bool includeSpecialCharacters = SpecialCharactersCheckBox.Checked;
+
+            if (!includeUppercase && !includeLowercase && !includeNumbers && !includeSpecialCharacters)
+            {
+                MessageBox.Show("Please select at least one character type.", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            string password = GeneratePassword(passwordLength, includeUppercase, includeLowercase, includeNumbers, includeSpecialCharacters);
+            PasswordTextBox.Text = password;
+        }
+
+        private string GeneratePassword(int length, bool includeUppercase, bool includeLowercase, bool includeNumbers, bool includeSpecialCharacters)
+        {
+            StringBuilder password = new StringBuilder(length);
+
+            string characterSet = "";
+            if (includeUppercase) characterSet += UppercaseLetters;
+            if (includeLowercase) characterSet += LowercaseLetters;
+            if (includeNumbers) characterSet += Numbers;
+            if (includeSpecialCharacters) characterSet += SpecialCharacters;
+
+            if (characterSet.Length == 0)
+                throw new ArgumentException("Character set cannot be empty.");
+
+            using (RandomNumberGenerator rng = RandomNumberGenerator.Create())
+            {
+                if (includeUppercase) password.Append(GetRandomCharacter(UppercaseLetters, rng));
+                if (includeLowercase) password.Append(GetRandomCharacter(LowercaseLetters, rng));
+                if (includeNumbers) password.Append(GetRandomCharacter(Numbers, rng));
+                if (includeSpecialCharacters) password.Append(GetRandomCharacter(SpecialCharacters, rng));
+
+                for (int i = password.Length; i < length; i++)
+                {
+                    password.Append(GetRandomCharacter(characterSet, rng));
+                }
+            }
+
+            return ShuffleString(password.ToString());
+        }
+
+        private char GetRandomCharacter(string characterSet, RandomNumberGenerator rng)
+        {
+            byte[] bytes = new byte[4];
+            rng.GetBytes(bytes);
+            int index = BitConverter.ToInt32(bytes, 0) & int.MaxValue;
+            return characterSet[index % characterSet.Length];
+        }
+
+        private string ShuffleString(string str)
+        {
+            char[] array = str.ToCharArray();
+            using (RandomNumberGenerator rng = RandomNumberGenerator.Create())
+            {
+                for (int i = array.Length - 1; i > 0; i--)
+                {
+                    byte[] bytes = new byte[4];
+                    rng.GetBytes(bytes);
+                    int j = (int)(BitConverter.ToUInt32(bytes, 0) % (i + 1));
+                    char temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+            return new string(array);
+        }
+
+        private void CopyPasswordButton_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(PasswordTextBox.Text))
+            {
+                Clipboard.SetText(PasswordTextBox.Text);
+                MessageBox.Show("Password copied to clipboard!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("No password to copy. Generate a password first.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
